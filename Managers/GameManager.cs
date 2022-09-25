0d967145d0797e06342da8229f84a9aa9b488dc2
medinak044 <63528703+medinak044@users.noreply.github.com>
2022-09-24 21:12:29 -0500

    Included enemy unit

--- Managers/GameManager.cs
+++ Managers/GameManager.cs
@@ -11,7 +11,7 @@ public class GameManager : Singleton<GameManager>
     [SerializeField] Unit enemyUnit;
 
     [SerializeField] TextMeshProUGUI playerUnitStatsText;
-    //[SerializeField] TextMeshProUGUI enemyUnitStatsText;
+    [SerializeField] TextMeshProUGUI enemyUnitStatsText;
 
     private void OnEnable()
     {
@@ -26,7 +26,23 @@ public class GameManager : Singleton<GameManager>
                 {
                     Id = "123",
                     Name = "Leif",
-                    HitPoints = 10
+                    HitPoints = 10,
+                    Attack = 1,
+                });
+            });
+        }
+
+        playerUnit = realm.Find<Unit>("567");
+        if (playerUnit == null)
+        {
+            realm.Write(() =>
+            {
+                playerUnit = realm.Add(new Unit()
+                {
+                    Id = "567",
+                    Name = "Lilina",
+                    HitPoints = 10,
+                    Attack = 1,
                 });
             });
         }
@@ -39,9 +55,9 @@ public class GameManager : Singleton<GameManager>
 
     void Start()
     {
-        UpdateStatsUI(playerUnit);
+        UpdateStatsUI(playerUnitStatsText, playerUnit);
+        UpdateStatsUI(enemyUnitStatsText, enemyUnit);
 
-        Debug.Log(playerUnit.Name);
     }
 
     void Update()
@@ -49,15 +65,33 @@ public class GameManager : Singleton<GameManager>
         
     }
 
-    void UpdateStatsUI(Unit unit)
+    void UpdateStatsUI(TextMeshProUGUI ui, Unit unit)
     {
-        playerUnitStatsText.text = 
+        ui.text = 
             $"Name: {unit.Name}\n" +
             $"HP: {unit.HitPoints}"; 
     }
 
-    public void SetHP(int num)
+    public void PlayerAttacks()
     {
-        playerUnit.HitPoints += num;
+        Attack(true, enemyUnit, playerUnit.Attack);
+    }
+
+    public void EnemyAttacks()
+    {
+        Attack(false, playerUnit, enemyUnit.Attack);
+    }
+
+    public void Attack(bool isPlayerUnit, Unit targetedUnit, int damage)
+    {
+        targetedUnit.HitPoints += -(damage - targetedUnit.Defense);
+        if (isPlayerUnit)
+        {
+            UpdateStatsUI(playerUnitStatsText, targetedUnit);
+        } 
+        else
+        {
+            UpdateStatsUI(enemyUnitStatsText, targetedUnit);
+        }
     }
 }
